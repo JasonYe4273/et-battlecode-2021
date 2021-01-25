c892a017cdddbb5986d65d001789374bf789d906
Anthony Grebe <agrebe@mit.edu>
2021-01-24 23:13:50 -0500

    Comment out all print statements

--- src/quals/Center.java
+++ src/quals/Center.java
@@ -67,7 +67,7 @@ public class Center extends Robot {
                 else neutralHQ = !enemyHQ;
             }
         }
-        System.out.println(enemyHQ + " " + neutralHQ);
+        //System.out.println(enemyHQ + " " + neutralHQ);
 
         // if we are attacking a neutral HQ, find the influence of the nearest neutral
         if (neutralHQ) {
@@ -175,9 +175,9 @@ public class Center extends Robot {
     
     // this method now receives edges as well as non-friendly HQs
     public void readNonfriendlyHQFlag() throws GameActionException {
-        System.out.println("Starting reading: " + Clock.getBytecodesLeft());
+        //System.out.println("Starting reading: " + Clock.getBytecodesLeft());
         Iterator<Integer> it = rakers.iterator();
-        System.out.println("num rakers " +rakers.size());
+        //System.out.println("num rakers " +rakers.size());
         while(it.hasNext()) {
             int id = it.next();
             if(rc.canGetFlag(id)) {
@@ -188,7 +188,7 @@ public class Center extends Robot {
                 it.remove();
             }
         }
-        System.out.println("Read rakers: " + Clock.getBytecodesLeft());
+        //System.out.println("Read rakers: " + Clock.getBytecodesLeft());
         it = others.iterator();
         while(it.hasNext()) {
             int id = it.next();
@@ -200,7 +200,7 @@ public class Center extends Robot {
                 it.remove();
             }
         }
-        System.out.println("Read others: " + Clock.getBytecodesLeft());
+        //System.out.println("Read others: " + Clock.getBytecodesLeft());
         sendNonfriendlyHQ();
     }
     int hqIndex=0;
