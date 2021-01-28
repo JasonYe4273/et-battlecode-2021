7d70bb4ee1dfc6ab6dcd980cad8627997b89b94f
Anthony Grebe <agrebe@mit.edu>
2021-01-27 23:04:42 -0500

    Removed objects to see if this would accelerate code, added check for whether location was occupied

--- src/finals/Dijkstra.java
+++ src/finals/Dijkstra.java
@@ -9,77 +9,84 @@ public class Dijkstra extends Navigation {
     System.out.println("Intelligent navigation to " + l);
     // initialize nearby locations
     MapLocation currentLoc = rc.getLocation();
-    LocationInfo [] nearbyLocs = new LocationInfo[DIAMETER * DIAMETER];
+    MapLocation [] nearbyLocs = new MapLocation[DIAMETER * DIAMETER];
+    double passabilities [] = new double[DIAMETER * DIAMETER];
+    double distances [] = new double[DIAMETER * DIAMETER];
+    boolean [] unoccupied = new boolean[DIAMETER * DIAMETER];
     for (int i = -RADIUS; i <= RADIUS; i ++) {
       for (int j = -RADIUS; j <= RADIUS; j ++) {
         MapLocation newLoc = currentLoc.translate(i, j);
         double passability, distToTarget;
-        boolean unoccupied;
+        boolean unoccupiedLoc;
         if (!rc.onTheMap(newLoc)) {
           passability = Double.MAX_VALUE;
           distToTarget = Double.MAX_VALUE;
-          unoccupied = false;
+          unoccupiedLoc = false;
         } else {
           passability = 1.0 / rc.sensePassability(newLoc);
-          distToTarget = Math.max(Math.abs(newLoc.x - l.x), Math.abs(newLoc.y - l.y)) * 100 + passability;
-          unoccupied = true;
+          distToTarget = Math.max(Math.abs(newLoc.x - l.x), Math.abs(newLoc.y - l.y)) * 100 + Math.sqrt(l.distanceSquaredTo(newLoc)) * 0.01 + passability;
+          unoccupiedLoc = !(rc.isLocationOccupied(newLoc));
         }
-        LocationInfo newLocationInfo = new LocationInfo(newLoc, passability, distToTarget, unoccupied);
-        nearbyLocs[(i+RADIUS)*DIAMETER+(j+RADIUS)] = newLocationInfo;
+        nearbyLocs[(i+RADIUS)*DIAMETER+(j+RADIUS)] = newLoc;
+        passabilities[(i+RADIUS)*DIAMETER+(j+RADIUS)] = passability;
+        distances[(i+RADIUS)*DIAMETER+(j+RADIUS)] = distToTarget;
+        unoccupied[(i+RADIUS)*DIAMETER+(j+RADIUS)] = unoccupiedLoc;
       }
     }
     System.out.println("Bytecodes used in checking location validity: " + (Clock.getBytecodeNum() - initialBytecodeCount));
-    MinHeap<LocationInfo> pq = new MinHeap<LocationInfo> (nearbyLocs);
+    MinHeap pq = new MinHeap (distances);
     System.out.println("Bytecodes used in checking location validity + initializing MinHeap: " + (Clock.getBytecodeNum() - initialBytecodeCount));
    
     // now implement Dijkstra on the priority queue
-    LocationInfo newLoc = null;
+    int newLocIndex = -2;
+    MapLocation newLoc = null;
     System.out.println(l);
-    while (newLoc == null || newLoc.loc != currentLoc) {
-      newLoc = pq.extractMin();
-      System.out.println(newLoc.loc + " " + newLoc.distToTarget);
-      if (newLoc == null) System.out.println("FATAL ERROR: newLoc should not be null!");
-      double newLocCost = newLoc.distToTarget; // cost of moving from newLoc to target
-      if (!newLoc.unoccupied) continue;
+    while (newLocIndex == -2 || nearbyLocs[newLocIndex] != currentLoc) {
+      newLocIndex = pq.extractMin();
+      newLoc = nearbyLocs[newLocIndex];
+      System.out.println(newLoc + " " + distances[newLocIndex]);
+      //if (newLoc == null) System.out.println("FATAL ERROR: newLoc should not be null!");
+      double newLocCost = distances[newLocIndex]; // cost of moving from newLoc to target
+      if (!unoccupied[newLocIndex]) continue;
       // update neighbors in nearbyLocs
-      int i = newLoc.loc.x - currentLoc.x;
-      int j = newLoc.loc.y - currentLoc.y;
+      int i = newLoc.x - currentLoc.x;
+      int j = newLoc.y - currentLoc.y;
       if (i <= 1 && i >= -1 && j <= 1 && j >= -1) break;
-      if (i-1 >= -RADIUS && nearbyLocs[(i-1+RADIUS)*DIAMETER + (j+RADIUS)].distToTarget > newLocCost + nearbyLocs[(i-1+RADIUS)*DIAMETER + (j+RADIUS)].passability) {
-        nearbyLocs[(i-1+RADIUS)*DIAMETER + (j+RADIUS)].distToTarget = newLocCost + nearbyLocs[(i-1+RADIUS)*DIAMETER + (j+RADIUS)].passability;
-        pq.updateKey((i-1+RADIUS)*DIAMETER + (j+RADIUS));
+      if (i-1 >= -RADIUS && distances[(i-1+RADIUS)*DIAMETER + (j+RADIUS)] > newLocCost + passabilities[(i-1+RADIUS)*DIAMETER + (j+RADIUS)]) {
+        distances[(i-1+RADIUS)*DIAMETER + (j+RADIUS)] = newLocCost + passabilities[(i-1+RADIUS)*DIAMETER + (j+RADIUS)];
+        pq.updateKey((i-1+RADIUS)*DIAMETER + (j+RADIUS), distances[(i-1+RADIUS)*DIAMETER + (j+RADIUS)]);
       }
-      if (i+1 <= RADIUS && nearbyLocs[(i+1+RADIUS)*DIAMETER + (j+RADIUS)].distToTarget > newLocCost + nearbyLocs[(i+1+RADIUS)*DIAMETER + (j+RADIUS)].passability) {
-        nearbyLocs[(i+1+RADIUS)*DIAMETER + (j+RADIUS)].distToTarget = newLocCost + nearbyLocs[(i+1+RADIUS)*DIAMETER + (j+RADIUS)].passability;
-        pq.updateKey((i+1+RADIUS)*DIAMETER + (j+RADIUS));
+      if (i+1 <= RADIUS && distances[(i+1+RADIUS)*DIAMETER + (j+RADIUS)] > newLocCost + passabilities[(i+1+RADIUS)*DIAMETER + (j+RADIUS)]) {
+        distances[(i+1+RADIUS)*DIAMETER + (j+RADIUS)] = newLocCost + passabilities[(i+1+RADIUS)*DIAMETER + (j+RADIUS)];
+        pq.updateKey((i+1+RADIUS)*DIAMETER + (j+RADIUS), distances[(i+1+RADIUS)*DIAMETER + (j+RADIUS)]);
       }
-      if (j-1 >= -RADIUS && nearbyLocs[(i+RADIUS)*DIAMETER + (j-1+RADIUS)].distToTarget > newLocCost + nearbyLocs[(i+RADIUS)*DIAMETER + (j-1+RADIUS)].passability) {
-        nearbyLocs[(i+RADIUS)*DIAMETER + (j-1+RADIUS)].distToTarget = newLocCost + nearbyLocs[(i+RADIUS)*DIAMETER + (j-1+RADIUS)].passability;
-        pq.updateKey((i+RADIUS)*DIAMETER + (j-1+RADIUS));
+      if (j-1 >= -RADIUS && distances[(i+RADIUS)*DIAMETER + (j-1+RADIUS)] > newLocCost + passabilities[(i+RADIUS)*DIAMETER + (j-1+RADIUS)]) {
+        distances[(i+RADIUS)*DIAMETER + (j-1+RADIUS)] = newLocCost + passabilities[(i+RADIUS)*DIAMETER + (j-1+RADIUS)];
+        pq.updateKey((i+RADIUS)*DIAMETER + (j-1+RADIUS), distances[(i+RADIUS)*DIAMETER + (j-1+RADIUS)]);
       }
-      if (j+1 <= RADIUS && nearbyLocs[(i+RADIUS)*DIAMETER + (j+1+RADIUS)].distToTarget > newLocCost + nearbyLocs[(i+RADIUS)*DIAMETER + (j+1+RADIUS)].passability) {
-        nearbyLocs[(i+RADIUS)*DIAMETER + (j+1+RADIUS)].distToTarget = newLocCost + nearbyLocs[(i+RADIUS)*DIAMETER + (j+1+RADIUS)].passability;
-        pq.updateKey((i+RADIUS)*DIAMETER + (j+1+RADIUS));
+      if (j+1 <= RADIUS && distances[(i+RADIUS)*DIAMETER + (j+1+RADIUS)] > newLocCost + passabilities[(i+RADIUS)*DIAMETER + (j+1+RADIUS)]) {
+        distances[(i+RADIUS)*DIAMETER + (j+1+RADIUS)] = newLocCost + passabilities[(i+RADIUS)*DIAMETER + (j+1+RADIUS)];
+        pq.updateKey((i+RADIUS)*DIAMETER + (j+1+RADIUS), distances[(i+RADIUS)*DIAMETER + (j+1+RADIUS)]);
       }
-      if (i-1 >= -RADIUS && j-1 >= -RADIUS && nearbyLocs[(i-1+RADIUS)*DIAMETER + (j-1+RADIUS)].distToTarget > newLocCost + nearbyLocs[(i-1+RADIUS)*DIAMETER + (j-1+RADIUS)].passability) {
-        nearbyLocs[(i-1+RADIUS)*DIAMETER + (j-1+RADIUS)].distToTarget = newLocCost + nearbyLocs[(i-1+RADIUS)*DIAMETER + (j-1+RADIUS)].passability;
-        pq.updateKey((i-1+RADIUS)*DIAMETER + (j-1+RADIUS));
+      if (i-1 >= -RADIUS && j-1 >= -RADIUS && distances[(i-1+RADIUS)*DIAMETER + (j-1+RADIUS)] > newLocCost + passabilities[(i-1+RADIUS)*DIAMETER + (j-1+RADIUS)]) {
+        distances[(i-1+RADIUS)*DIAMETER + (j-1+RADIUS)] = newLocCost + passabilities[(i-1+RADIUS)*DIAMETER + (j-1+RADIUS)];
+        pq.updateKey((i-1+RADIUS)*DIAMETER + (j-1+RADIUS), distances[(i-1+RADIUS)*DIAMETER + (j-1+RADIUS)]);
       }
-      if (i+1 <= RADIUS && j-1 >= -RADIUS && nearbyLocs[(i+1+RADIUS)*DIAMETER + (j-1+RADIUS)].distToTarget > newLocCost + nearbyLocs[(i+1+RADIUS)*DIAMETER + (j-1+RADIUS)].passability) {
-        nearbyLocs[(i+1+RADIUS)*DIAMETER + (j-1+RADIUS)].distToTarget = newLocCost + nearbyLocs[(i+1+RADIUS)*DIAMETER + (j-1+RADIUS)].passability;
-        pq.updateKey((i+1+RADIUS)*DIAMETER + (j-1+RADIUS));
+      if (i+1 <= RADIUS && j-1 >= -RADIUS && distances[(i+1+RADIUS)*DIAMETER + (j-1+RADIUS)] > newLocCost + passabilities[(i+1+RADIUS)*DIAMETER + (j-1+RADIUS)]) {
+        distances[(i+1+RADIUS)*DIAMETER + (j-1+RADIUS)] = newLocCost + passabilities[(i+1+RADIUS)*DIAMETER + (j-1+RADIUS)];
+        pq.updateKey((i+1+RADIUS)*DIAMETER + (j-1+RADIUS), distances[(i+1+RADIUS)*DIAMETER + (j-1+RADIUS)]);
       }
-      if (i-1 >= -RADIUS && j+1 <= RADIUS && nearbyLocs[(i-1+RADIUS)*DIAMETER + (j+1+RADIUS)].distToTarget > newLocCost + nearbyLocs[(i-1+RADIUS)*DIAMETER + (j+1+RADIUS)].passability) {
-        nearbyLocs[(i-1+RADIUS)*DIAMETER + (j+1+RADIUS)].distToTarget = newLocCost + nearbyLocs[(i-1+RADIUS)*DIAMETER + (j+1+RADIUS)].passability;
-        pq.updateKey((i-1+RADIUS)*DIAMETER + (j+1+RADIUS));
+      if (i-1 >= -RADIUS && j+1 <= RADIUS && distances[(i-1+RADIUS)*DIAMETER + (j+1+RADIUS)] > newLocCost + passabilities[(i-1+RADIUS)*DIAMETER + (j+1+RADIUS)]) {
+        distances[(i-1+RADIUS)*DIAMETER + (j+1+RADIUS)] = newLocCost + passabilities[(i-1+RADIUS)*DIAMETER + (j+1+RADIUS)];
+        pq.updateKey((i-1+RADIUS)*DIAMETER + (j+1+RADIUS), distances[(i-1+RADIUS)*DIAMETER + (j+1+RADIUS)]);
       }
-      if (i+1 <= RADIUS && j+1 <= RADIUS && nearbyLocs[(i+1+RADIUS)*DIAMETER + (j+1+RADIUS)].distToTarget > newLocCost + nearbyLocs[(i+1+RADIUS)*DIAMETER + (j+1+RADIUS)].passability) {
-        nearbyLocs[(i+1+RADIUS)*DIAMETER + (j+1+RADIUS)].distToTarget = newLocCost + nearbyLocs[(i+1+RADIUS)*DIAMETER + (j+1+RADIUS)].passability;
-        pq.updateKey((i+1+RADIUS)*DIAMETER + (j+1+RADIUS));
+      if (i+1 <= RADIUS && j+1 <= RADIUS && distances[(i+1+RADIUS)*DIAMETER + (j+1+RADIUS)] > newLocCost + passabilities[(i+1+RADIUS)*DIAMETER + (j+1+RADIUS)]) {
+        distances[(i+1+RADIUS)*DIAMETER + (j+1+RADIUS)] = newLocCost + passabilities[(i+1+RADIUS)*DIAMETER + (j+1+RADIUS)];
+        pq.updateKey((i+1+RADIUS)*DIAMETER + (j+1+RADIUS), distances[(i+1+RADIUS)*DIAMETER + (j+1+RADIUS)]);
       }
     }
     if (newLoc != null) {
-      Direction dir = currentLoc.directionTo(newLoc.loc);
+      Direction dir = currentLoc.directionTo(newLoc);
       if (rc.canMove(dir)) rc.move(dir);
     }
     System.out.println("Total bytecodes used: " + (Clock.getBytecodeNum() - initialBytecodeCount));
--- src/finals/MinHeap.java
+++ src/finals/MinHeap.java
@@ -1,14 +1,14 @@
 package finals;
 
-public class MinHeap<T extends Comparable<T>> {
-  T [] objects;
+public class MinHeap {
+  double [] keys;
   int [] newIndices; // newIndices[i] returns the new index of the object originally in index i
   int [] oldIndices; // oldIndices[i] returns the old index of the object currently in index i
   // note that newIndices[oldIndices[i]] = oldIndices[newIndices[i]] = i
   int n;
-  public MinHeap (T [] objects) {
-    n = objects.length;
-    this.objects = objects.clone();
+  public MinHeap (double [] keys) {
+    n = keys.length;
+    this.keys = keys.clone();
     newIndices = new int[n];
     oldIndices = new int[n];
     for (int i = 0; i < n; i ++) newIndices[i] = oldIndices[i] = i;
@@ -19,9 +19,9 @@ public class MinHeap<T extends Comparable<T>> {
   // swaps objects in indices i and j and updates the indices array accordingly
   public void swap (int i, int j) {
     // swap the objects
-    T tmpObj = objects[i];
-    objects[i] = objects[j];
-    objects[j] = tmpObj;
+    double tmpKey = keys[i];
+    keys[i] = keys[j];
+    keys[j] = tmpKey;
     // swap the new indices at oldIndices[i] and oldIndices[j]
     newIndices[oldIndices[i]] = j;
     newIndices[oldIndices[j]] = i;
@@ -38,18 +38,18 @@ public class MinHeap<T extends Comparable<T>> {
   public void minHeapify(int i) {
     if (2*i >= n-1) return; // i has no children
     else if (2*i == n-2) { // i has only a left child
-      if (objects[i].compareTo(objects[2*i+1]) > 0) {
+      if (keys[i] > keys[2*i+1]) {
         swap(i, 2*i+1);
         minHeapify(2*i+1);
       }
     } else { // i has both left and right children
-      if (objects[2*i+1].compareTo(objects[2*i+2]) < 0) {
-        if (objects[i].compareTo(objects[2*i+1]) > 0) {
+      if (keys[2*i+1] < keys[2*i+2]) {
+        if (keys[i] > keys[2*i+1]) {
           swap(i, 2*i+1);
           minHeapify(2*i+1);
         }
       } else {
-        if (objects[i].compareTo(objects[2*i+2]) > 0) {
+        if (keys[i] > keys[2*i+2]) {
           swap(i, 2*i+2);
           minHeapify(2*i+2);
         }
@@ -57,21 +57,23 @@ public class MinHeap<T extends Comparable<T>> {
     }
   }
 
-  public T extractMin() {
-    if (n == 0) return null;
-    T minObject = objects[0];
-    objects[0] = objects[n-1];
+  // return the old index of the minimum element in the heap
+  public int extractMin() {
+    if (n == 0) return -1;
+    int minObject = oldIndices[0];
+    swap(0, n-1);
     n--;
     minHeapify(0);
     return minObject;
   }
 
   // fix the heap after indices[originalIndex] has an updated key (smaller than the original)
-  public void updateKey(int originalIndex) {
+  public void updateKey(int originalIndex, double newKey) {
     int i = newIndices[originalIndex];
+    keys[i] = newKey;
     while (i > 0) {
       int parent = (i-1)/2;
-      if (objects[i].compareTo(objects[parent]) < 0) swap(i, parent);
+      if (keys[i] < keys[parent]) swap(i, parent);
       i = parent;
     }
   }
